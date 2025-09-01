4ec290998e87789bdbeb55b65eec1b7330c8761e
Oleksandr Hroskopf <oleksandrhroskopf@MacBook-Air-Oleksandr-2.local>
2025-09-02 01:17:42 +0300

    typo

--- MazeSolver.hs
+++ MazeSolver.hs
@@ -2,9 +2,9 @@ module MazeSolver (shortestPath) where
 
 import Maze
 
-neighbours :: GraphList -> Cell -> [Cell]
-neighbours ((v, vs):graph) x| x == v      = vs
-                            | otherwise   = neighbours graph x
+neighbors :: GraphList -> Cell -> [Cell]
+neighbors ((v, vs):graph) x| x == v      = vs
+                            | otherwise   = neighbors graph x
 
 addToQueue :: [Cell] -> [[Cell]] -> [Cell] -> [[Cell]]
 addToQueue [] queue _ = queue
@@ -16,4 +16,4 @@ shortestPath :: GraphList -> Cell -> Cell -> [Cell]
 shortestPath graph start finish = bfs graph finish [] [[start]] 
                         where bfs graph finish visited ((v:path):queue) | v == finish       = (v:path)       
                                                                         | v `elem` visited  = bfs graph finish visited queue
-                                                                        | otherwise         = bfs graph finish (v:visited) (addToQueue (neighbours graph v) queue (v:path))
+                                                                        | otherwise         = bfs graph finish (v:visited) (addToQueue (neighbors graph v) queue (v:path))
