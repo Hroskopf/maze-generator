a79df34a2e5c94299af78b0cf81ff86e8a002b9b
Oleksandr Hroskopf <oleksandrhroskopf@MacBook-Air-Oleksandr-2.local>
2025-09-04 00:28:25 +0300

    adding edges (for cyclic mazes)

--- Gen.hs
+++ Gen.hs
@@ -1,8 +1,10 @@
-module Gen(dfsGen) where
+module Gen(dfsGen, addRandomEdges) where
 
 import System.Random.Shuffle (shuffleM)
 import Control.Monad (foldM)
 import Data.List (sort)
+import Debug.Trace (traceShow)
+
 
 import Maze
 import MazeSolver
@@ -17,14 +19,21 @@ addDirectedEdge ((v, v_neigh):graph) x y| x == v     = ((v, (y:v_neigh)):graph)
 addEdge :: GraphList -> Cell -> Cell -> GraphList
 addEdge graph x y = addDirectedEdge (addDirectedEdge graph x y) y x
 
+addEdges :: GraphList -> [(Cell, Cell)] -> GraphList
+addEdges g [] = g
+addEdges g ((x, y):rest) = addEdges (addEdge g x y) rest
 
+emptyGrid :: Int -> Int -> GraphList
+emptyGrid height width = [((x, y), []) | x <- [0.. height - 1], y <- [0.. width - 1]]
 
-dfsGen :: Int -> Int -> Float -> IO Graph
-dfsGen height width difficulty = do
+dfsGen :: Int -> Int -> Float -> Float -> IO Graph
+dfsGen height width difficulty sparsity = do
     (g, _) <- dfs (emptyGrid height width) [] (-1, -1) (0, 0)
     let difficulty_int = floor (difficulty * (fromIntegral (width * width - 1)))
-    let (start, finish) = chooseStartAndFinish g height width difficulty_int
-    return (Graph height width g start finish)            
+    let edges_to_add = floor (sparsity * (fromIntegral ((height * width) - height - width + 1)))
+    graph <- addRandomEdges g height width edges_to_add
+    let (start, finish) = chooseStartAndFinish graph height width difficulty_int
+    return (Graph height width graph start finish)            
             where
                 dfs :: GraphList -> [Cell] -> Cell -> Cell -> IO (GraphList, [Cell]) 
                 dfs graph visited parent v| v `elem` visited   =  return (graph, visited)
@@ -33,13 +42,20 @@ dfsGen height width difficulty = do
                                                 foldM(\(gr, vis) n -> dfs (if parent == (-1, -1) then gr else (addEdge gr parent v)) (v:vis) v n) (graph, visited) ns
 
 
-emptyGrid :: Int -> Int -> GraphList
-emptyGrid height width = [((x, y), []) | x <- [0.. height - 1], y <- [0.. width - 1]]
-
 chooseStartAndFinish :: GraphList -> Int -> Int -> Int -> (Cell, Cell)
-chooseStartAndFinish graph height width num = (c1, c2) where
-                (_, c1, c2) = (sort shortestPaths) !! num 
-                    where cells_up = [(0, x) | x <- [0..(width - 1)]]
-                          cells_down   = [(height - 1, x) | x <- [0..(width - 1)]]
-                          shortestPaths = [(dist, x, y) | x <- cells_up, (y, dist) <- (shortestPathsLength graph x cells_down)]
-                          
+chooseStartAndFinish graph height width num = (c1, c2)
+  where
+    (_, c1, c2) = (sort debuggedPaths) !! num
+    cells_up    = [(0, x) | x <- [0..(width - 1)]]
+    cells_down  = [(height - 1, x) | x <- [0..(width - 1)]]
+    shortestPaths =
+      [(dist, x, y) | x <- cells_up, (y, dist) <- (shortestPathsLength graph x cells_down)]
+    debuggedPaths = traceShow shortestPaths shortestPaths
+
+
+addRandomEdges :: GraphList -> Int -> Int -> Int -> IO GraphList
+addRandomEdges graph height width number = do   
+                        let notEdges = [(x, y) | (x, ns) <- graph, y <- (neighbors x height width), not (y `elem` ns), x < y]
+                        edgesShuffled <- shuffleM notEdges
+                        let edgesToAdd = take number edgesShuffled
+                        return (addEdges graph edgesToAdd)
